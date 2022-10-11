3be4fba45d4fe4f5787a0e9f6e753fa155eb00c7
yooseong12 <rladbtjd81@gmail.com>
2022-10-11 18:09:23 +0900

    프로젝트 생성

---
+++ src/test/java/com/example/usedshop/UsedShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.usedshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsedShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
